32d78f6986e88aa5a21791e72a2ecebc5328b40a
Matt Dennebaum <matt@ninjacipher.com>
2012-07-28 15:01:48 -0400

    added optional timeout to memcached set

--- whirlwind/db/memcache_interface.py
+++ whirlwind/db/memcache_interface.py
@@ -2,31 +2,34 @@ import memcache
 from whirlwind.util.singleton import Singleton
 
 class Memcache(Singleton):
-    db = None
-    
-    def __init__(self):
-        self.pool = None
-        
-    @staticmethod
-    def create(**kwargs):
-        mc = memcache.Client(kwargs['host'].split(','), debug=0)
-        if 'debug' in kwargs:
-            print kwargs
-            print mc
-        
-        Memcache.db = mc
-        print Memcache.db
-        
-    @staticmethod
-    def get(key):
-        if Memcache.db == None:
-            return None
-        else:
-            return Memcache.db.get(key)
-        
-    @staticmethod
-    def set(key,val):
-        if Memcache.db == None:
-            return False
-        else:
-            return Memcache.db.set(key,val)
+	db = None
+	
+	def __init__(self):
+		self.pool = None
+		
+	@staticmethod
+	def create(**kwargs):
+		mc = memcache.Client(kwargs['host'].split(','), debug=0)
+		if 'debug' in kwargs:
+			print kwargs
+			print mc
+		
+		Memcache.db = mc
+		print Memcache.db
+		
+	@staticmethod
+	def get(key):
+		if Memcache.db == None:
+			return None
+		else:
+			return Memcache.db.get(key)
+		
+	@staticmethod
+	def set(key,val,timeout=None):
+		if Memcache.db == None:
+			return False
+		else:
+			if timeout:
+				return Memcache.db.set(key,val,timeout)
+			else:
+				return Memcache.db.set(key,val)
