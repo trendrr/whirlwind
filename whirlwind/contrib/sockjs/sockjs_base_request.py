308cb2dffaf8e6cce17b33f65b9a7f5c0f1f17a2
Matt Dennebaum <matt@ninjacipher.com>
2012-09-26 15:46:41 -0400

    updated sockjs base request

--- whirlwind/contrib/sockjs/sockjs_base_request.py
+++ whirlwind/contrib/sockjs/sockjs_base_request.py
@@ -1,25 +1,12 @@
 from sockjs.tornado import SockJSConnection
-from whirlwind.middleware import MiddlewareManager
 from whirlwind.db.mongo import Mongo
 import json
 
 class SockjsBaseRequest(SockJSConnection):
 
-	def __init__(self, sjssession):
+	def __init__(self, sjsession):
 		SockJSConnection.__init__(self, sjssession)
-		self._current_user = None
-		self.middleware_manager = MiddlewareManager(self)
 		self.db = Mongo.db.ui
-		self.middleware_manager.run_request_hooks()
-
-	def get_current_user(self):
-		return self._current_user
-
-	def set_current_user(self, user):
-		self._current_user = user
-
-	def is_logged_in(self):
-		return self.get_current_user() != None
 
 	def send_message(self,message):
 		if isinstance(message, basestring):
