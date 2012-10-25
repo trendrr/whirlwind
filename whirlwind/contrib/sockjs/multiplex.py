6827355cbaf7ab1d5f52c63924f669693685cc12
Matt Dennebaum <matt@ninjacipher.com>
2012-10-25 10:58:37 -0400

    another sockjs fix

--- whirlwind/contrib/sockjs/multiplex.py
+++ whirlwind/contrib/sockjs/multiplex.py
@@ -9,7 +9,7 @@ class ChannelSession(session.BaseSession):
 		self.base = base
 		self.name = name
 
-	def send_message(self, msg, binary=False):
+	def send_message(self, msg, stats=True, binary=False):
 		self.base.send('msg,' + self.name + ',' + msg)
 
 	def on_message(self, msg):
